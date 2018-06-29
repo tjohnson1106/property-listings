eaed21c98aa59182307ccb5961dc87e77a0d714e
Thomas Johnson <tjohnson1106@gmail.com>
2018-06-29 13:26:34 -0400

    added expo project with typescript configuration

--- packages/web/src/apollo.ts
+++ packages/web/src/apollo.ts
@@ -2,9 +2,11 @@ import { ApolloClient } from "apollo-client";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { HttpLink } from "apollo-link-http";
 
+//  for dev environment: "http://localhost:4000"
+
 export const client = new ApolloClient({
   link: new HttpLink({
-    uri: "http://localhost:4000",
+    uri: process.env.REACT_APP_SERVER_URL,
     credentials: "include"
   }),
   cache: new InMemoryCache()
