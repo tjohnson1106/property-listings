a86c74cc0c7e4011fdc42afeb190c39597d346a7
Thomas Johnson <tjohnson1106@gmail.com>
2018-07-05 17:52:09 -0400

    added basic scaffolding for mobile input field component

--- packages/app/src/modules/shared/InputField.tsx
+++ packages/app/src/modules/shared/InputField.tsx
@@ -1,25 +1,33 @@
 import * as React from "react";
-import {FieldProps} from "formik";
-import {Form, Input} from "antd";
+import Component = React.Component;
+import { FieldProps } from "formik";
+import { Input } from "react-native-elements";
 
-const FormItem = Form.Item;
+//left 07/05 install: rn-elenments beta-5 , remove old types
+//onchange text function next
 
-export const InputField: React.SFC<
-  FieldProps<any> & {prefix: React.ReactNode}
-> = ({
-  field, // { name, value, onChange, onBlur }
-  form: {touched, errors}, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
-  ...props
-}) => {
-  const errorMessage = touched[field.name] && errors[field.name];
+class InputField extends Component<FieldProps<any>> {
+  render() {
+    const {
+      field, // { name, value, onChange, onBlur }
+      form: { touched, errors }, // also values, setXXXX, handleXXXX, dirty, isValid, status, etc.
+      ...props
+    } = this.props;
+    {
+      const errorMessage = touched[field.name] && errors[field.name];
 
-  return (
-    <FormItem
-      help={errorMessage}
-      // tslint:disable-next-line:jsx-no-multiline-js
-      validateStatus={errorMessage ? "error" : undefined}
-    >
-      <Input {...field} {...props} />
-    </FormItem>
-  );
-};
+      return (
+        // <FormItem
+        //   help={errorMessage}
+        //   // tslint:disable-next-line:jsx-no-multiline-js
+        //   validateStatus={errorMessage ? "error" : undefined}
+        // >
+        //   <Input {...field} {...props} />
+        // </FormItem>
+        <Input />
+      );
+    }
+  }
+}
+
+export default InputField;
