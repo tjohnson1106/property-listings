acf658d5bcb8135d112f258c69e60780dfa036c2
Thomas Johnson <tjohnson1106@gmail.com>
2018-07-13 13:19:03 -0400

    added transporter credential variable

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -20,3 +20,35 @@ export const sendEmail = async (recipient: string, url: string) => {
   });
   console.log(response);
 };
+
+import * as nodemailer from "nodemailer";
+
+//credentials
+const transporter = nodemailer.createTransport({
+  host: "smtp.ethereal.email",
+  port: 587,
+  auth: {
+    user: "fozn67fxhjcotlyo@ethereal.email",
+    pass: "x53xFd5pqUJwJvHW7p"
+  }
+});
+
+// Message object
+let message = {
+  from: "Sender Name <sender@example.com>",
+  to: "Recipient <recipient@example.com>",
+  subject: "Nodemailer is unicode friendly ✔",
+  text: "Hello to myself!",
+  html: "<p><b>Hello</b> to myself!</p>"
+};
+
+transporter.sendMail(message, (err, info) => {
+  if (err) {
+    console.log("Error occurred. " + err.message);
+    return process.exit(1);
+  }
+
+  console.log("Message sent: %s", info.messageId);
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+});
