2fac20daed46b368c0eb2c60be3bca4b3e3623ed
Thomas Johnson <tjohnson1106@gmail.com>
2018-07-13 15:10:06 -0400

    added test email to register resolver

--- packages/server/src/utils/sendEmail.ts
+++ packages/server/src/utils/sendEmail.ts
@@ -1,54 +1,39 @@
-import * as SparkPost from "sparkpost";
-const client = new SparkPost(process.env.SPARKPOST_API_KEY);
-
-export const sendEmail = async (recipient: string, url: string) => {
-  const response = await client.transmissions.send({
-    options: {
-      sandbox: true
-    },
-    content: {
-      from: "testing@sparkpostbox.com",
-      subject: "Confirm Email",
-      html: `<html>
-        <body>
-        <p>Testing SparkPost - the world's most awesomest email service!</p>
-        <a href="${url}">confirm email</a>
-        </body>
-        </html>`
-    },
-    recipients: [{ address: recipient }]
-  });
-  console.log(response);
-};
-
 import * as nodemailer from "nodemailer";
 
-//credentials
-const transporter = nodemailer.createTransport({
-  host: "smtp.ethereal.email",
-  port: 587,
-  auth: {
-    user: "fozn67fxhjcotlyo@ethereal.email",
-    pass: "x53xFd5pqUJwJvHW7p"
-  }
-});
+export const sendEmail = async (recipient: string, url: string) => {
+  //credentials
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: "fozn67fxhjcotlyo@ethereal.email",
+      pass: "x53xFd5pqUJwJvHW7p"
+    }
+  });
 
-// Message object
-let message = {
-  from: "Sender Name <sender@example.com>",
-  to: "Recipient <recipient@example.com>",
-  subject: "Nodemailer is unicode friendly ✔",
-  text: "Hello to myself!",
-  html: "<p><b>Hello</b> to myself!</p>"
+  const message = {
+    from: "Sender Name <sender@example.com>",
+    to: `Recepient <${recipient}>`,
+    subject: "Nodemailer is unicode friendly ✔",
+    text: "Hello to myself!",
+    html: `<html>
+  <body>
+  <p>Testing NodeMailer /p>
+  <a href="${url}">confirm email</a>
+  </body>
+  </html>`
+  };
+
+  transporter.sendMail(message, (err, info) => {
+    if (err) {
+      console.log("Error occurred. " + err.message);
+    }
+
+    console.log("Message sent: %s", info.messageId);
+    // Preview only available when sending through an Ethereal account
+    console.log(
+      "Preview URL: %s",
+      nodemailer.getTestMessageUrl(info)
+    );
+  });
 };
-
-transporter.sendMail(message, (err, info) => {
-  if (err) {
-    console.log("Error occurred. " + err.message);
-    return process.exit(1);
-  }
-
-  console.log("Message sent: %s", info.messageId);
-  // Preview only available when sending through an Ethereal account
-  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
-});
