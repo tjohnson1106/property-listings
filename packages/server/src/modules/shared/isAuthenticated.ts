874c0bc7e2bec2d897c1560c7867069695f972b6
Thomas Johnson <tjohnson1106@gmail.com>
2018-07-31 14:57:31 -0400

    created is authentciated function and added to create and delete

---
+++ packages/server/src/modules/shared/isAuthenticated.ts
@@ -0,0 +1,9 @@
+import { Session } from "../../types/graphql-utils";
+
+export const isAuthenticated = (session: Session) => {
+  if (!session.userId) {
+    //user is not logged in
+
+    throw new Error("not authenticated");
+  }
+};
