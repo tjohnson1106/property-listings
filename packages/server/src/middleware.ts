bf4652972f784ba32711b3f213659966b8edd5e3
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-01 13:34:44 -0400

    added deleting listing authentication via gqlmiddleware

--- packages/server/src/middleware.ts
+++ packages/server/src/middleware.ts
@@ -1,13 +1,22 @@
+const isAuthenticated = async (
+  resolve: any,
+  parent: any,
+  args: any,
+  context: any,
+  info: any
+) => {
+  if (!context.session.userId) {
+    //user is not logged in
+
+    throw new Error("not authenticated from graphql middlware");
+  }
+
+  return resolve(parent, args, context, info);
+};
+
 export const middleware = {
   Mutation: {
-    createListing: async (
-      resolve: any,
-      parent: any,
-      args: any,
-      context: any,
-      info: any
-    ) => {
-      return resolve(parent, args, context, info);
-    }
+    createListing: isAuthenticated,
+    deleteListing: isAuthenticated
   }
 };
