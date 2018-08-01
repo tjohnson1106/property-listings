f96bc7b14d3903224ac5610c008d26295a892bdf
Thomas Johnson <tjohnson1106@gmail.com>
2018-08-01 14:49:17 -0400

    added auth checks for is user logged in and loading screen

--- packages/controller/src/modules/auth/AuthRouter.tsx
+++ packages/controller/src/modules/auth/AuthRouter.tsx
@@ -1,15 +1,41 @@
 import * as React from "react";
 import PureComponent = React.PureComponent;
-import { RouteProps } from "react-router";
+import {
+  RouteProps,
+  Route,
+  RouteComponentProps,
+  Redirect
+} from "react-router";
 import { graphql, ChildProps } from "react-apollo";
 import gql from "graphql-tag";
 import { MeQuery } from "../..";
 
 type Props = RouteProps;
 
-export class PreAuth extends PureComponent<ChildProps> {
+class PreAuth extends PureComponent<ChildProps<Props, MeQuery>> {
+  renderRoute = (routeProps: RouteComponentProps<{}>) => {
+    const { data, component } = this.props;
+
+    if (!data || !data.loading) {
+      //loading screen
+
+      return null;
+    }
+
+    if (!data.me) {
+      //user not logged in
+
+      return <Redirect to="/login" />;
+    }
+
+    const Component = component as any;
+
+    return <Component {...routeProps} />;
+  };
+
   render() {
-    return {};
+    const { data: _, component: __, ...rest } = this.props;
+    return <Route {...rest} render={this.renderRoute} />;
   }
 }
 
